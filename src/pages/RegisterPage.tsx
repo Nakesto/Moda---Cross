631346869820760ab5042e4d4dfc62340e78dccc
RommyKJ <rommy.kj54@gmail.com>
2022-11-27 21:33:11 +0700

    Responsive Select, Login,Register
    Sudah bisa register,tapi belom bisa login

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,76 +1,236 @@
-import { IonAvatar, IonBackButton, IonButton, IonButtons, IonCheckbox, IonContent, IonDatetime, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonModal, IonNote, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonLabel,
+  IonPage,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
 import './RegisterPage.css'
-import { useRef, useState } from 'react';
-import { mail,  calendar } from 'ionicons/icons';
-import { FaTransgender} from "react-icons/fa";
-import { BsCameraFill, BsFillTelephoneFill } from "react-icons/bs";
-import { RiLockPasswordFill } from "react-icons/ri";
-import { HiIdentification } from "react-icons/hi";
-import { AiTwotoneSecurityScan } from "react-icons/ai";
-import profile from '../Assets/tom_holland.jpg'
+import { useRef, useState } from 'react'
+import { mail, calendar } from 'ionicons/icons'
+import { FaTransgender } from 'react-icons/fa'
+import { BsCameraFill, BsFillTelephoneFill } from 'react-icons/bs'
+import { RiLockPasswordFill } from 'react-icons/ri'
+import { HiIdentification } from 'react-icons/hi'
+import { AiTwotoneSecurityScan } from 'react-icons/ai'
+import profile from '../Assets/profile.png'
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { useHistory } from 'react-router'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 
 const RegisterPage: React.FC = () => {
-    const inputEmail = useRef<HTMLIonInputElement>(null);
+  const inputEmail = useRef<HTMLIonInputElement>(null)
+  const inputNama = useRef<HTMLIonInputElement>(null)
+  const inputPhone = useRef<HTMLIonInputElement>(null)
+  const inputPassword = useRef<HTMLIonInputElement>(null)
+  const inputDate = useRef<HTMLIonInputElement>(null)
+  const inputPin = useRef<HTMLIonInputElement>(null)
+  const [takenPhoto, setTakenPhoto] = useState<string>()
+  const [selectedfile, setSelectedFile] = useState<File>()
+  const [fileName, setFileName] = useState('')
+  const [gender, setGender] = useState<'male' | 'female'>('male')
+  const storage = getStorage()
+  const db = getFirestore()
+  const history = useHistory()
+  const selectGender = (event: CustomEvent) => {
+    const selectedGender = event.detail.value
+    setGender(selectedGender)
+  }
 
-    return (
-        <IonPage>
-        <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Fill Your Profile</IonTitle>
-                    <IonButtons slot="start">
-                        <IonBackButton />
-                    </IonButtons>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <div className='container-img'>
-                  <div className="profilepic">
-                    <img className="profilepic_image" src={profile} width="150" height="150" alt="Profibild" />
-                    <div className="profilepic_content">
-                      <span className="profilepic_icon"><BsCameraFill className='fas'/></span>
-                      <span className="profilepic_text">Edit Profile</span>
-                    </div>
-                  </div>
-                </div>
-                <IonGrid className='login-group'>
-                    <div  className="input-item">
-                        <IonLabel><HiIdentification className='input-icon' /></IonLabel>
-                        <IonInput  className='input-text' placeholder='Full Name' type="text"></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><IonIcon className='input-icon' slot="start" icon={mail}/></IonLabel>
-                        <IonInput  className='input-text' placeholder='Email' type="email"></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><IonIcon className='input-icon' slot="start" icon={calendar}/></IonLabel>
-                        <IonInput className="input-date" placeholder="Date of Birth" type='date'></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><FaTransgender className='input-icon'/></IonLabel>
-                        <IonSelect className='gender-dropdown' interface="popover" placeholder="Select Gender">
-                          <IonSelectOption className="item-dropdown" value="male">Male</IonSelectOption>
-                          <IonSelectOption className="item-dropdown"  value="female">Female</IonSelectOption>
-                        </IonSelect>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><BsFillTelephoneFill className='input-icon'  /></IonLabel>
-                        <IonInput  className='input-text' placeholder='Phone Number' type="tel" maxlength={13}></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><RiLockPasswordFill className='input-icon'  /></IonLabel>
-                        <IonInput  className='input-text' placeholder='Password' type="password"></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><AiTwotoneSecurityScan className='input-icon'  /></IonLabel>
-                        <IonInput  className='input-text' placeholder='Pin (Number Only 4 Digits)' pattern="[0-9]+" maxlength={4} type="password"></IonInput>
-                    </div>
-                    <IonRow>
-                        <button  className='btn-login'>Sign Up</button>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    );
-};
+  const email = inputEmail.current?.value as string
+  const password = inputPassword.current?.value as string
 
-export default RegisterPage;
+  const fileChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(event.target!.files![0])
+    setFileName(event.target!.files![0].name)
+    setTakenPhoto(URL.createObjectURL(event.target!.files![0]))
+  }
+
+  const addData = async (url: string) => {
+    const valGender = gender as string
+    try {
+      const docRef = await addDoc(collection(db, 'user'), {
+        name: inputNama.current?.value as string,
+        email: email,
+        birthdate: inputDate.current?.value as string,
+        gender: valGender,
+        phone: inputPhone.current?.value as number,
+        password: password,
+        pin: inputPin.current?.value as number,
+        photoUrl: url,
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const insertHandler = async () => {
+    const storageRef = ref(storage, fileName)
+    uploadBytes(storageRef, selectedfile as Blob).then((snapshot) => {
+      getDownloadURL(ref(storage, fileName)).then((url) => {
+        addData(url)
+        history.push('/login')
+      })
+    })
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Fill Your Profile</IonTitle>
+          <IonButtons slot="start">
+            <IonBackButton />
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="container-img">
+          <div className="profilepic">
+            {takenPhoto && (
+              <img
+                className="profilepic_image"
+                src={takenPhoto}
+                width="150"
+                height="150"
+                alt="Profibild"
+              />
+            )}
+            {!takenPhoto && (
+              <img
+                className="profilepic_image"
+                src={profile}
+                width="150"
+                height="150"
+                alt="Profibild"
+              />
+            )}
+            <div className="profilepic_content">
+              <label htmlFor="actual-btn">
+                <span className="profilepic_icon">
+                  <BsCameraFill className="fas" />
+                </span>
+                <span className="profilepic_text">Edit Profile</span>
+              </label>
+              <input
+                id="actual-btn"
+                type="file"
+                onChange={fileChangeHandler}
+                hidden
+              />
+            </div>
+          </div>
+        </div>
+        <div className="login-group">
+          <div className="input-item-register">
+            <IonLabel>
+              <HiIdentification className="input-icon" />
+            </IonLabel>
+            <IonInput
+              className="input-text"
+              placeholder="Full Name"
+              type="text"
+              ref={inputNama}
+            ></IonInput>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <IonIcon className="input-icon" slot="start" icon={mail} />
+            </IonLabel>
+            <IonInput
+              className="input-text"
+              placeholder="Email"
+              type="email"
+              ref={inputEmail}
+            ></IonInput>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <IonIcon className="input-icon" slot="start" icon={calendar} />
+            </IonLabel>
+            <IonInput
+              className="input-date"
+              placeholder="Date of Birth"
+              type="date"
+              ref={inputDate}
+            ></IonInput>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <FaTransgender className="input-icon" />
+            </IonLabel>
+            <IonSelect
+              className="gender-dropdown"
+              interface="popover"
+              placeholder="Select Gender"
+              onIonChange={selectGender}
+              value={gender}
+            >
+              <IonSelectOption className="item-dropdown" value="male">
+                Male
+              </IonSelectOption>
+              <IonSelectOption className="item-dropdown" value="female">
+                Female
+              </IonSelectOption>
+            </IonSelect>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <BsFillTelephoneFill className="input-icon" />
+            </IonLabel>
+            <IonInput
+              className="input-text"
+              placeholder="Phone Number"
+              type="number"
+              ref={inputPhone}
+            ></IonInput>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <RiLockPasswordFill className="input-icon" />
+            </IonLabel>
+            <IonInput
+              className="input-text"
+              placeholder="Password"
+              type="password"
+              ref={inputPassword}
+            ></IonInput>
+          </div>
+          <div className="input-item-register">
+            <IonLabel>
+              <AiTwotoneSecurityScan className="input-icon" />
+            </IonLabel>
+            <IonInput
+              className="input-text"
+              placeholder="Pin (Number Only 4 Digits)"
+              max={4}
+              type="number"
+              ref={inputPin}
+            ></IonInput>
+          </div>
+          <IonRow>
+            <button
+              style={{ marginTop: '10px', marginBottom: '5px' }}
+              className="btn-login"
+              onClick={insertHandler}
+            >
+              Sign Up
+            </button>
+          </IonRow>
+        </div>
+      </IonContent>
+    </IonPage>
+  )
+}
+
+export default RegisterPage
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,61 +1,112 @@
-import { IonBackButton, IonButton, IonButtons, IonCheckbox, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonNote, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import './loginPage.css';
-import { useRef, useState } from 'react';
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import './loginPage.css'
+import { useRef, useState } from 'react'
 import loginImg from '../Assets/login.png'
-import { mail, lockClosed } from 'ionicons/icons';
-import { FcGoogle } from "react-icons/fc";
-import { FaApple, FaFacebook} from "react-icons/fa";
-import { MdOutgoingMail } from "react-icons/md";
+import { mail, lockClosed } from 'ionicons/icons'
+import { FcGoogle } from 'react-icons/fc'
+import { FaApple, FaFacebook } from 'react-icons/fa'
+import { getAuth } from 'firebase/auth'
 
 const LoginPage: React.FC = () => {
-    const inputEmail = useRef<HTMLIonInputElement>(null);
-    return (
-        <IonPage>
-        <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Sign in With Email</IonTitle>
-                    <IonButtons slot="start">
-                        <IonBackButton />
-                    </IonButtons>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <div className="img-container">
-                    <img className="img-login" src={loginImg}/>
-                </div>
-                <h1 className='h1-login'><strong>Login to Your Account</strong></h1>
-                <IonGrid className='login-group'>
-                    <div  className="input-item">
-                        <IonLabel><IonIcon className='input-icon' slot="start" icon={mail}/></IonLabel>
-                        <IonInput  className='input-text' placeholder='Email' type="email"></IonInput>
-                    </div>
-                    <div  className="input-item">
-                        <IonLabel><IonIcon className='input-icon' slot="start" icon={lockClosed}/></IonLabel>
-                        <IonInput  className='input-text' placeholder='Password' type="password"></IonInput>
-                    </div>
-                    <IonItem lines='none'>
-                        <IonCheckbox slot="start"></IonCheckbox>
-                        <IonLabel>Remember Me</IonLabel>
-                    </IonItem>
-                    <IonRow>
-                        <button  className='btn-login'>Sign In</button>
-                    </IonRow>
-                    <div style={{display: 'flex', width:'350px', flexDirection: 'row', alignItems: 'center', paddingRight: '.5rem', paddingLeft: '.5rem'}}>
-                        <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
-                        <div>
-                            <p style={{width: '140px', textAlign: 'center'}}>or continue with</p>
-                        </div>
-                        <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
-                    </div>
-                    <IonRow>
-                        <IonButton fill="outline" className='btn-icon-login'><FaFacebook /></IonButton>
-                        <IonButton fill="outline" className='btn-icon-login'><FcGoogle/></IonButton>
-                        <IonButton fill="outline" className='btn-icon-login'><FaApple style={{color: 'black'}}/></IonButton>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    );
-};
+  const inputEmail = useRef<HTMLIonInputElement>(null)
+  return (
+    <IonPage className="container">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Sign in With Email</IonTitle>
+          <IonButtons slot="start">
+            <IonBackButton />
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="container-login">
+          <img className="img-login" src={loginImg} />
+          <h1 className="h1-login">
+            <strong>Login to Your Account</strong>
+          </h1>
+          <div className="login-group">
+            <div className="input-item">
+              <IonLabel>
+                <IonIcon className="input-icon" slot="start" icon={mail} />
+              </IonLabel>
+              <IonInput
+                className="input-text"
+                placeholder="Email"
+                type="email"
+              ></IonInput>
+            </div>
+            <div className="input-item">
+              <IonLabel>
+                <IonIcon
+                  className="input-icon"
+                  slot="start"
+                  icon={lockClosed}
+                />
+              </IonLabel>
+              <IonInput
+                className="input-text"
+                placeholder="Password"
+                type="password"
+              ></IonInput>
+            </div>
+            <IonItem lines="none">
+              <IonCheckbox slot="start"></IonCheckbox>
+              <IonLabel>Remember Me</IonLabel>
+            </IonItem>
+            <IonRow>
+              <button className="btn-login">Sign In</button>
+            </IonRow>
+            <div className="line-1">
+              <div
+                style={{ flex: 1, height: '1px', backgroundColor: 'black' }}
+              />
+              <div>
+                <p style={{ width: '140px', textAlign: 'center' }}>
+                  or continue with
+                </p>
+              </div>
+              <div
+                style={{ flex: 1, height: '1px', backgroundColor: 'black' }}
+              />
+            </div>
+            <IonRow>
+              <IonButton fill="outline" className="btn-icon-login">
+                <FaFacebook style={{ width: '50px', height: '30px' }} />
+              </IonButton>
+              <IonButton fill="outline" className="btn-icon-login">
+                <FcGoogle style={{ width: '50px', height: '30px' }} />
+              </IonButton>
+              <IonButton
+                fill="outline"
+                style={{ paddingRight: '0px' }}
+                className="btn-icon-login"
+              >
+                <FaApple
+                  style={{ color: 'black', width: '50px', height: '30px' }}
+                />
+              </IonButton>
+            </IonRow>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
