25cfddd13f0f4982c0e4a519ee2f8cb3acaad326
RommyKJ <rommy.kj54@gmail.com>
2022-11-28 23:09:20 +0700

    Auth login & register
    Login register auth login dan register (facebook, email, google). Apple belom

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -15,79 +15,83 @@ import {
   IonToolbar,
 } from '@ionic/react'
 import './RegisterPage.css'
-import { useRef, useState } from 'react'
-import { mail, calendar } from 'ionicons/icons'
+import { useEffect, useRef, useState } from 'react'
+import { mail, calendar, attach } from 'ionicons/icons'
 import { FaTransgender } from 'react-icons/fa'
 import { BsCameraFill, BsFillTelephoneFill } from 'react-icons/bs'
 import { RiLockPasswordFill } from 'react-icons/ri'
 import { HiIdentification } from 'react-icons/hi'
 import { AiTwotoneSecurityScan } from 'react-icons/ai'
 import profile from '../Assets/profile.png'
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { addDoc, collection } from 'firebase/firestore'
 import { useHistory } from 'react-router'
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+import { app, storage, auth, db } from '../firebase'
 
 const RegisterPage: React.FC = () => {
-  const inputEmail = useRef<HTMLIonInputElement>(null)
-  const inputNama = useRef<HTMLIonInputElement>(null)
-  const inputPhone = useRef<HTMLIonInputElement>(null)
-  const inputPassword = useRef<HTMLIonInputElement>(null)
-  const inputDate = useRef<HTMLIonInputElement>(null)
-  const inputPin = useRef<HTMLIonInputElement>(null)
   const [takenPhoto, setTakenPhoto] = useState<string>()
   const [selectedfile, setSelectedFile] = useState<File>()
-  const [fileName, setFileName] = useState('')
+  const [fileName, setFileName] = useState('profile.png')
   const [gender, setGender] = useState<'male' | 'female'>('male')
-  const storage = getStorage()
-  const db = getFirestore()
-  const history = useHistory()
+  const [errorEmail, setErrorEmail] = useState<string>()
   const selectGender = (event: CustomEvent) => {
     const selectedGender = event.detail.value
     setGender(selectedGender)
   }
+  const history = useHistory()
 
-  const email = inputEmail.current?.value as string
-  const password = inputPassword.current?.value as string
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
 
   const fileChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedFile(event.target!.files![0])
     setFileName(event.target!.files![0].name)
     setTakenPhoto(URL.createObjectURL(event.target!.files![0]))
   }
-
-  const addData = async (url: string) => {
-    const valGender = gender as string
-    try {
-      const docRef = await addDoc(collection(db, 'user'), {
-        name: inputNama.current?.value as string,
-        email: email,
-        birthdate: inputDate.current?.value as string,
-        gender: valGender,
-        phone: inputPhone.current?.value as number,
-        password: password,
-        pin: inputPin.current?.value as number,
-        photoUrl: url,
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  const insertHandler = async () => {
+  const onSubmit = (data: any) => {
+    setErrorEmail('')
     const storageRef = ref(storage, fileName)
     uploadBytes(storageRef, selectedfile as Blob).then((snapshot) => {
       getDownloadURL(ref(storage, fileName)).then((url) => {
         addData(url)
-        history.push('/login')
       })
     })
+
+    const addData = async (url: string) => {
+      const valGender = gender as string
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          data.email as string,
+          data.password as string,
+        )
+        const docRef = await addDoc(collection(db, 'user'), {
+          name: data.fullname as string,
+          email: data.email as string,
+          birthdate: data.birthdate as string,
+          gender: valGender,
+          phone: data.phone as number,
+          password: data.password,
+          pin: data.pin as number,
+          photoUrl: url,
+        })
+        history.push('/login')
+      } catch (error) {
+        setErrorEmail('Email already registered!')
+      }
+    }
   }
 
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonTitle>Fill Your Profile</IonTitle>
           <IonButtons slot="start">
             <IonBackButton />
@@ -95,139 +99,224 @@ const RegisterPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <div className="container-img">
-          <div className="profilepic">
-            {takenPhoto && (
-              <img
-                className="profilepic_image"
-                src={takenPhoto}
-                width="150"
-                height="150"
-                alt="Profibild"
-              />
-            )}
-            {!takenPhoto && (
-              <img
-                className="profilepic_image"
-                src={profile}
-                width="150"
-                height="150"
-                alt="Profibild"
-              />
-            )}
-            <div className="profilepic_content">
-              <label htmlFor="actual-btn">
-                <span className="profilepic_icon">
-                  <BsCameraFill className="fas" />
-                </span>
-                <span className="profilepic_text">Edit Profile</span>
-              </label>
-              <input
-                id="actual-btn"
-                type="file"
-                onChange={fileChangeHandler}
-                hidden
-              />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="container-img">
+            <div className="profilepic">
+              {takenPhoto && (
+                <img
+                  className="profilepic_image"
+                  src={takenPhoto}
+                  width="150"
+                  height="150"
+                  alt="Profibild"
+                />
+              )}
+              {!takenPhoto && (
+                <img
+                  className="profilepic_image"
+                  src={profile}
+                  width="150"
+                  height="150"
+                  alt="Profibild"
+                />
+              )}
+              <div className="profilepic_content">
+                <label htmlFor="actual-btn">
+                  <span className="profilepic_icon">
+                    <BsCameraFill className="fas" />
+                  </span>
+                  <span className="profilepic_text">Edit Profile</span>
+                </label>
+                <input
+                  id="actual-btn"
+                  type="file"
+                  onChange={fileChangeHandler}
+                  hidden
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="login-group">
-          <div className="input-item-register">
-            <IonLabel>
-              <HiIdentification className="input-icon" />
-            </IonLabel>
-            <IonInput
-              className="input-text"
-              placeholder="Full Name"
-              type="text"
-              ref={inputNama}
-            ></IonInput>
+          <div className="login-group">
+            <div className="input-item-register">
+              <IonLabel>
+                <HiIdentification className="input-icon" />
+              </IonLabel>
+              <IonInput
+                {...register('fullname', {
+                  required: 'This is a required field',
+                  minLength: {
+                    value: 3,
+                    message: 'Name cannot be less than 3 chars!',
+                  },
+                })}
+                placeholder="Fullname"
+                className="input-text"
+                name="fullname"
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="fullname"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <div className="input-item-register">
+              <IonLabel>
+                <IonIcon className="input-icon" slot="start" icon={mail} />
+              </IonLabel>
+              <IonInput
+                {...register('email', {
+                  required: 'This is a required field',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: 'Invalid email address',
+                  },
+                })}
+                placeholder="Email"
+                className="input-text"
+                name="email"
+              />
+            </div>
+            <div className="error-message" style={{ color: 'red' }}>
+              {errorEmail}
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="email"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <div className="input-item-register">
+              <IonLabel>
+                <IonIcon className="input-icon" slot="start" icon={calendar} />
+              </IonLabel>
+              <IonInput
+                {...register('birthdate', {
+                  required: 'This is a required field',
+                })}
+                className="input-date"
+                placeholder="Birthdate"
+                type="date"
+                name="birthdate"
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="birthdate"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <div className="input-item-register">
+              <IonLabel>
+                <FaTransgender className="input-icon" />
+              </IonLabel>
+              <IonSelect
+                className="gender-dropdown"
+                interface="popover"
+                placeholder="Select Gender"
+                onIonChange={selectGender}
+                value={gender}
+              >
+                <IonSelectOption className="item-dropdown" value="male">
+                  Male
+                </IonSelectOption>
+                <IonSelectOption className="item-dropdown" value="female">
+                  Female
+                </IonSelectOption>
+              </IonSelect>
+            </div>
+            <div className="input-item-register">
+              <IonLabel>
+                <BsFillTelephoneFill className="input-icon" />
+              </IonLabel>
+              <IonInput
+                {...register('phone', {
+                  required: 'This is a required field',
+                  minLength: {
+                    value: 11,
+                    message: 'Phone number cannot less than 11 number!',
+                  },
+                  maxLength: {
+                    value: 13,
+                    message: 'Phone number cannot more than 13 number!',
+                  },
+                })}
+                className="input-text"
+                placeholder="Phone Number"
+                type="number"
+                name="phone"
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="phone"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <div className="input-item-register">
+              <IonLabel>
+                <RiLockPasswordFill className="input-icon" />
+              </IonLabel>
+              <IonInput
+                {...register('password', {
+                  required: 'This is a required field',
+                  minLength: {
+                    value: 8,
+                    message: 'Password cannot less than 8 chars!',
+                  },
+                  pattern: {
+                    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+                    message: 'Password must be have uppercase and number!',
+                  },
+                })}
+                className="input-text"
+                placeholder="Password"
+                type="password"
+                name="password"
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <div className="input-item-register">
+              <IonLabel>
+                <AiTwotoneSecurityScan className="input-icon" />
+              </IonLabel>
+              <IonInput
+                {...register('pin', {
+                  required: 'This is a required field',
+                  pattern: {
+                    value: /[0-9]/,
+                    message: 'Pin must be number!',
+                  },
+                  minLength: {
+                    value: 4,
+                    message: 'Pin must be 4 numbers!',
+                  },
+                  maxLength: {
+                    value: 4,
+                    message: 'Pin must be 4 numbers!',
+                  },
+                })}
+                className="input-text"
+                placeholder="Pin (4 Numbers)"
+                type="password"
+                name="pin"
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="pin"
+              as={<div className="error-message" style={{ color: 'red' }} />}
+            />
+            <IonRow>
+              <button
+                style={{ marginTop: '10px', marginBottom: '5px' }}
+                className="btn-login"
+              >
+                Sign Up
+              </button>
+            </IonRow>
           </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <IonIcon className="input-icon" slot="start" icon={mail} />
-            </IonLabel>
-            <IonInput
-              className="input-text"
-              placeholder="Email"
-              type="email"
-              ref={inputEmail}
-            ></IonInput>
-          </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <IonIcon className="input-icon" slot="start" icon={calendar} />
-            </IonLabel>
-            <IonInput
-              className="input-date"
-              placeholder="Date of Birth"
-              type="date"
-              ref={inputDate}
-            ></IonInput>
-          </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <FaTransgender className="input-icon" />
-            </IonLabel>
-            <IonSelect
-              className="gender-dropdown"
-              interface="popover"
-              placeholder="Select Gender"
-              onIonChange={selectGender}
-              value={gender}
-            >
-              <IonSelectOption className="item-dropdown" value="male">
-                Male
-              </IonSelectOption>
-              <IonSelectOption className="item-dropdown" value="female">
-                Female
-              </IonSelectOption>
-            </IonSelect>
-          </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <BsFillTelephoneFill className="input-icon" />
-            </IonLabel>
-            <IonInput
-              className="input-text"
-              placeholder="Phone Number"
-              type="number"
-              ref={inputPhone}
-            ></IonInput>
-          </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <RiLockPasswordFill className="input-icon" />
-            </IonLabel>
-            <IonInput
-              className="input-text"
-              placeholder="Password"
-              type="password"
-              ref={inputPassword}
-            ></IonInput>
-          </div>
-          <div className="input-item-register">
-            <IonLabel>
-              <AiTwotoneSecurityScan className="input-icon" />
-            </IonLabel>
-            <IonInput
-              className="input-text"
-              placeholder="Pin (Number Only 4 Digits)"
-              max={4}
-              type="number"
-              ref={inputPin}
-            ></IonInput>
-          </div>
-          <IonRow>
-            <button
-              style={{ marginTop: '10px', marginBottom: '5px' }}
-              className="btn-login"
-              onClick={insertHandler}
-            >
-              Sign Up
-            </button>
-          </IonRow>
-        </div>
+        </form>
       </IonContent>
     </IonPage>
   )
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -13,21 +13,104 @@ import {
   IonRow,
   IonTitle,
   IonToolbar,
+  IonBadge,
 } from '@ionic/react'
 import './loginPage.css'
-import { useRef, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import loginImg from '../Assets/login.png'
-import { mail, lockClosed } from 'ionicons/icons'
+import { lockClosed, mailUnread } from 'ionicons/icons'
 import { FcGoogle } from 'react-icons/fc'
 import { FaApple, FaFacebook } from 'react-icons/fa'
-import { getAuth } from 'firebase/auth'
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from 'firebase/auth'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router'
+import { ErrorMessage } from '@hookform/error-message'
+import { auth, providerFacebook, providerGoogle } from '../firebase'
+import { UserProvider } from '../context/UserData'
+
+export type DataUser = {
+  token: string
+  name: string
+  email: string
+  phoneNumber: number
+  gender: string
+  photoUrl: string
+}
 
 const LoginPage: React.FC = () => {
-  const inputEmail = useRef<HTMLIonInputElement>(null)
+  const history = useHistory()
+  const [userDat, setUserData] = useState<DataUser[]>([])
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+  const onSubmit = (data: any) => {
+    signInWithEmailAndPassword(
+      auth,
+      data.email as string,
+      data.password as string,
+    )
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        console.log(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const loginFacebook = () => {
+    signInWithPopup(auth, providerFacebook)
+      .then((result) => {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result)
+        if (credential !== null) {
+          const token = credential.accessToken
+
+          const user = result.user
+          history.push('/register')
+        }
+      })
+      .catch((error) => {
+        const credential = FacebookAuthProvider.credentialFromError(error)
+        console.log(credential)
+      })
+  }
+
+  const loginGoogle = () => {
+    signInWithPopup(auth, providerGoogle)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        if (credential !== null) {
+          const token = credential.accessToken
+
+          const user = result.user
+          console.log(user)
+        } else {
+          setUserData([])
+        }
+      })
+      .catch((error) => {
+        const credential = GoogleAuthProvider.credentialFromError(error)
+        console.log(credential)
+      })
+  }
+
+  const loginApple = () => {}
   return (
     <IonPage className="container">
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonTitle>Sign in With Email</IonTitle>
           <IonButtons slot="start">
             <IonBackButton />
@@ -41,37 +124,66 @@ const LoginPage: React.FC = () => {
             <strong>Login to Your Account</strong>
           </h1>
           <div className="login-group">
-            <div className="input-item">
-              <IonLabel>
-                <IonIcon className="input-icon" slot="start" icon={mail} />
-              </IonLabel>
-              <IonInput
-                className="input-text"
-                placeholder="Email"
-                type="email"
-              ></IonInput>
-            </div>
-            <div className="input-item">
-              <IonLabel>
-                <IonIcon
-                  className="input-icon"
-                  slot="start"
-                  icon={lockClosed}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="input-item">
+                <IonLabel>
+                  <IonIcon
+                    className="input-icon"
+                    slot="start"
+                    icon={mailUnread}
+                  />
+                </IonLabel>
+                <IonInput
+                  {...register('email', {
+                    required: 'This is a required field',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: 'Invalid email address',
+                    },
+                  })}
+                  placeholder="Email"
+                  name="email"
                 />
-              </IonLabel>
-              <IonInput
-                className="input-text"
-                placeholder="Password"
-                type="password"
-              ></IonInput>
-            </div>
-            <IonItem lines="none">
-              <IonCheckbox slot="start"></IonCheckbox>
-              <IonLabel>Remember Me</IonLabel>
-            </IonItem>
-            <IonRow>
-              <button className="btn-login">Sign In</button>
-            </IonRow>
+              </div>
+              <ErrorMessage
+                errors={errors}
+                name="email"
+                as={<div className="error-message" style={{ color: 'red' }} />}
+              />
+              <div className="input-item">
+                <IonLabel>
+                  <IonIcon
+                    className="input-icon"
+                    slot="start"
+                    icon={lockClosed}
+                  />
+                </IonLabel>
+                <IonInput
+                  {...register('password', {
+                    required: 'This is a required field',
+                  })}
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                />
+              </div>
+              <div className="error-message">
+                <ErrorMessage
+                  errors={errors}
+                  name="password"
+                  as={
+                    <div className="error-message" style={{ color: 'red' }} />
+                  }
+                />
+              </div>
+              <div className="check-box">
+                <IonCheckbox style={{ marginRight: '5px' }}></IonCheckbox>
+                <IonLabel>Remember Me!</IonLabel>
+              </div>
+              <IonRow>
+                <button className="btn-login">Sign In</button>
+              </IonRow>
+            </form>
             <div className="line-1">
               <div
                 style={{ flex: 1, height: '1px', backgroundColor: 'black' }}
@@ -86,10 +198,18 @@ const LoginPage: React.FC = () => {
               />
             </div>
             <IonRow>
-              <IonButton fill="outline" className="btn-icon-login">
+              <IonButton
+                fill="outline"
+                className="btn-icon-login"
+                onClick={loginFacebook}
+              >
                 <FaFacebook style={{ width: '50px', height: '30px' }} />
               </IonButton>
-              <IonButton fill="outline" className="btn-icon-login">
+              <IonButton
+                fill="outline"
+                className="btn-icon-login"
+                onClick={loginGoogle}
+              >
                 <FcGoogle style={{ width: '50px', height: '30px' }} />
               </IonButton>
               <IonButton
