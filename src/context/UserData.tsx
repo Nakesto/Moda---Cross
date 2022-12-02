37b2355fcbf3988c092ac91fbeb19f405c96378e
Nakesto <vinboyhuang88@gmail.com>
2022-12-02 14:08:32 +0700

    Authentication Done

--- src/context/UserData.tsx
+++ src/context/UserData.tsx
@@ -1,14 +1,16 @@
-import React, { createContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
 import { DataUser } from "../pages/loginPage";
 
 export type DataContext = {
-  userData: DataUser[];
+  userData: DataUser | null;
   isLoggedIn: boolean;
   loggedIn: () => void;
 };
 
 const initialValue: DataContext = {
-  userData: [],
+  userData: null,
   isLoggedIn: false,
   loggedIn: () => {},
 };
@@ -17,6 +19,18 @@ const UserContext = createContext(initialValue);
 
 const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [init, setInit] = useState(initialValue);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setInit({ ...init, isLoggedIn: true });
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [init]);
 
   const loggedIn = () => {
     setInit({
