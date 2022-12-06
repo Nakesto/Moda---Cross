117731dd8cd998c4d65d01aaebe44763f4c5c2c0
RommyKJ <rommy.kj54@gmail.com>
2022-12-06 23:46:38 +0700

    mantap bro

--- src/components/CardHero.tsx
+++ src/components/CardHero.tsx
@@ -1,56 +1,56 @@
-import { IonButton, IonText } from "@ionic/react";
-import React from "react";
-import Tom from "../Assets/tom_holland.jpg";
+import { IonButton, IonText } from '@ionic/react'
+import React from 'react'
+import Tom from '../Assets/tom_holland.jpg'
 
 const CardHero = () => {
   return (
     <div
       style={{
-        width: "100%",
-        backgroundColor: "white",
-        display: "flex",
-        alignItems: "center",
-        gap: "20px",
-        paddingLeft: "5px",
-        paddingRight: "5px",
+        width: '100%',
+        backgroundColor: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '20px',
+        paddingLeft: '5px',
+        paddingRight: '5px',
       }}
     >
       <img
-        src={Tom}
+        src="https://lzd-img-global.slatic.net/g/p/c4cfb0200286f9a6536b19fdb9b206e1.png_720x720q80.jpg_.webp"
         alt="Baju"
         width="150px"
         height="125px"
         style={{
-          borderRadius: "15px",
+          borderRadius: '15px',
         }}
       />
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
+          display: 'flex',
+          flexDirection: 'column',
         }}
       >
         <IonText
           style={{
-            fontWeight: "400",
-            fontSize: "20px",
-            marginBottom: "5px",
+            fontWeight: '400',
+            fontSize: '20px',
+            marginBottom: '5px',
           }}
         >
-          Introduction
+          Sale!
         </IonText>
         <IonText
           style={{
-            fontWeight: "600",
-            fontSize: "25px",
+            fontWeight: '600',
+            fontSize: '25px',
           }}
         >
-          H&M Shirt
+          Pull & Bear
         </IonText>
         <IonButton size="small">Buy Now</IonButton>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CardHero;
+export default CardHero
