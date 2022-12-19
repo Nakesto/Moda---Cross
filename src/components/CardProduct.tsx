0ed41c6a3e7994b363044bb1f38a4bfc231f15c4
Nakesto <vinboyhuang88@gmail.com>
2022-12-19 19:19:36 +0700

    rupiah, cart, and payment

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,4 +1,4 @@
-import { IonIcon, IonText, useIonToast } from '@ionic/react'
+import { IonIcon, IonText, useIonToast } from "@ionic/react";
 import {
   collection,
   doc,
@@ -6,114 +6,111 @@ import {
   query,
   updateDoc,
   where,
-} from 'firebase/firestore'
-import { add, checkmarkDoneOutline } from 'ionicons/icons'
-import React, { useContext } from 'react'
-import { useHistory } from 'react-router'
-import { UserContext } from '../context/UserData'
-import { db } from '../firebase'
-import { Product } from '../pages/Home'
-import './CardProduct.css'
+} from "firebase/firestore";
+import { add, checkmarkDoneOutline } from "ionicons/icons";
+import React, { useContext } from "react";
+import { useHistory } from "react-router";
+import { UserContext } from "../context/UserData";
+import { db } from "../firebase";
+import { rupiah } from "../pages/Cart";
+import { Product } from "../pages/Home";
+import "./CardProduct.css";
 
 const CardProduct = ({ product }: { product: Product }) => {
-  const [present] = useIonToast()
+  const [present] = useIonToast();
 
-  const presentToast = (position: 'bottom') => {
+  const presentToast = (position: "bottom") => {
     present({
-      message: 'Add product to cart success!',
+      message: "Add product to cart success!",
       duration: 1500,
       position: position,
       icon: checkmarkDoneOutline,
-      cssClass: 'custom-toast',
-    })
-  }
-  const { userData } = useContext(UserContext)
-  const history = useHistory()
+      cssClass: "custom-toast",
+    });
+  };
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
   const addCart = async (product: Product) => {
     try {
       const q = query(
-        collection(db, 'cart'),
-        where(
-          userData!.uid + product.uid + '.product.name',
-          '==',
-          product.name,
-        ),
-      )
-      const res = await getDocs(q)
-      let data: any = null
+        collection(db, "cart"),
+        where(userData!.uid + product.uid + ".product.name", "==", product.name)
+      );
+      const res = await getDocs(q);
+      let data: any = null;
       res.forEach((doc) => {
-        data = doc.data()
-      })
+        data = doc.data();
+      });
 
       if (data !== null) {
-        data = Object.entries(data)
-        await updateDoc(doc(db, 'cart', userData!.uid), {
-          [userData!.uid + product.uid + '.product']: product,
-          [userData!.uid + product.uid + '.quantity']: data[0][1].quantity + 1,
-        })
-        presentToast('bottom')
+        data = Object.entries(data);
+        await updateDoc(doc(db, "cart", userData!.uid), {
+          [userData!.uid + product.uid + ".product"]: product,
+          [userData!.uid + product.uid + ".quantity"]: data[0][1].quantity + 1,
+        });
+        presentToast("bottom");
       } else {
-        await updateDoc(doc(db, 'cart', userData!.uid), {
-          [userData!.uid + product.uid + '.product']: product,
-          [userData!.uid + product.uid + '.quantity']: 1,
-        })
-        presentToast('bottom')
+        await updateDoc(doc(db, "cart", userData!.uid), {
+          [userData!.uid + product.uid + ".product"]: product,
+          [userData!.uid + product.uid + ".quantity"]: 1,
+        });
+        presentToast("bottom");
       }
     } catch (err) {}
-  }
+  };
 
   const goDetailProduk = (product: Product) => {
-    history.push('/detailProduct', { product })
-  }
+    history.push("/detailProduct", { product });
+  };
 
   return (
     <div
       className="card-product"
       onClick={() => {
-        goDetailProduk(product)
+        goDetailProduk(product);
       }}
       style={{
-        cursor: 'pointer',
+        cursor: "pointer",
       }}
     >
       <img src={product.image} className="card-image" />
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
         }}
       >
         <IonText
           style={{
-            marginBottom: '5px',
+            marginBottom: "5px",
           }}
         >
           {product.name}
         </IonText>
         <IonText
           style={{
-            marginBottom: '15px',
+            marginBottom: "15px",
           }}
           onClick={() => {
-            goDetailProduk(product)
+            goDetailProduk(product);
           }}
         >
           {product.toko.name}
         </IonText>
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'space-between',
+            display: "flex",
+            justifyContent: "space-between",
           }}
         >
-          <IonText>{'Rp.' + product.price}</IonText>
+          <IonText>{rupiah(parseInt(product.price))}</IonText>
           <button
             onClick={(e) => {
-              e.stopPropagation()
-              addCart(product)
+              e.stopPropagation();
+              addCart(product);
             }}
             style={{
-              padding: '5px',
+              padding: "5px",
               zIndex: 10,
             }}
           >
@@ -122,7 +119,7 @@ const CardProduct = ({ product }: { product: Product }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardProduct
+export default CardProduct;
--- src/components/ListCart.tsx
+++ src/components/ListCart.tsx
@@ -1,63 +1,64 @@
-import { IonButton, IonCard, IonItem, IonLabel, IonRow } from '@ionic/react'
-import React, { useContext } from 'react'
-import produk from '../Assets/produk.png'
-import minus from '../Assets/minus.png'
-import plus from '../Assets/plus.png'
-import { Product } from '../pages/Home'
-import { deleteField, doc, updateDoc } from 'firebase/firestore'
-import { UserContext } from '../context/UserData'
-import { db } from '../firebase'
+import { IonButton, IonCard, IonItem, IonLabel, IonRow } from "@ionic/react";
+import React, { useContext } from "react";
+import produk from "../Assets/produk.png";
+import minus from "../Assets/minus.png";
+import plus from "../Assets/plus.png";
+import { Product } from "../pages/Home";
+import { deleteField, doc, updateDoc } from "firebase/firestore";
+import { UserContext } from "../context/UserData";
+import { db } from "../firebase";
+import { rupiah } from "../pages/Cart";
 
 const ListCart = ({
   product,
   quantity,
   trigger,
 }: {
-  product: Product
-  quantity: number
-  trigger: () => void
+  product: Product;
+  quantity: number;
+  trigger: () => void;
 }) => {
-  const { userData } = useContext(UserContext)
+  const { userData } = useContext(UserContext);
   const plussCart = async () => {
-    await updateDoc(doc(db, 'cart', userData!.uid), {
-      [userData!.uid + product.uid + '.quantity']: quantity + 1,
-    })
-  }
+    await updateDoc(doc(db, "cart", userData!.uid), {
+      [userData!.uid + product.uid + ".quantity"]: quantity + 1,
+    });
+  };
 
   const minusCart = async () => {
     if (quantity === 1) {
-      const cartRef = doc(db, 'cart', userData!.uid)
+      const cartRef = doc(db, "cart", userData!.uid);
 
       // Remove the 'capital' field from the document
       await updateDoc(cartRef, {
         [userData!.uid + product.uid]: deleteField(),
-      })
+      });
 
-      trigger()
-      return
+      trigger();
+      return;
     }
 
-    await updateDoc(doc(db, 'cart', userData!.uid), {
-      [userData!.uid + product.uid + '.quantity']: quantity - 1,
-    })
-  }
+    await updateDoc(doc(db, "cart", userData!.uid), {
+      [userData!.uid + product.uid + ".quantity"]: quantity - 1,
+    });
+  };
 
   return (
     <IonItem className="cardlist" lines="none" button>
       <img
         style={{
-          height: '100px',
-          width: '120px',
-          borderRadius: '10px',
-          marginRight: '10px',
+          height: "100px",
+          width: "120px",
+          borderRadius: "10px",
+          marginRight: "10px",
         }}
         alt="produk"
         src={product.image}
       />
 
-      <IonLabel style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+      <IonLabel style={{ paddingTop: "10px", paddingBottom: "10px" }}>
         <h2 className="des">{product.name}</h2>
-        <h3 className="des">{'Rp' + product.price}</h3>
+        <h3 className="des">{rupiah(parseInt(product.price))}</h3>
         <IonRow>
           <IonButton className="btn">
             <img src={minus} alt="" onClick={minusCart} />
@@ -69,7 +70,7 @@ const ListCart = ({
         </IonRow>
       </IonLabel>
     </IonItem>
-  )
-}
+  );
+};
 
-export default ListCart
+export default ListCart;
